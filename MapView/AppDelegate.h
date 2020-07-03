e83ce6c18dca081e6df224b50be06a3d9efadd87
IgorGalimski <igorg@playtika.com>
2020-07-03 10:58:51 +0300

    Initial Commit

---
+++ MapView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapView
+//
+//  Created by Igor Galimski on 7/3/20.
+//  Copyright © 2020 Igor Galimski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MapViewTests/MapViewTests.m
@@ -0,0 +1,37 @@
+//
+//  MapViewTests.m
+//  MapViewTests
+//
+//  Created by Igor Galimski on 7/3/20.
+//  Copyright © 2020 Igor Galimski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapViewTests : XCTestCase
+
+@end
+
+@implementation MapViewTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
